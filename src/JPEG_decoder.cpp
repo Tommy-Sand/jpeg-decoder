31ad8cde943f8be7d9437b9875f9daa0ace5e881
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-08-22 18:36:12 -0600

    Fixed Multiple issues.

--- src/JPEG_decoder.cpp
+++ src/JPEG_decoder.cpp
@@ -4,6 +4,8 @@
 #include <filesystem>
 #include <cassert>
 #include <vector>
+#include <cstdio>
+#include <cstdlib>
 
 uint8_t cur_byte;
 uint8_t cur_marker;
@@ -24,21 +26,43 @@ public:
     std::vector<std::vector<std::vector<uint8_t>>> thumb;
 };
 
+class QTable{
+public:
+    QTable(uint16_t Length, uint8_t Type, uint8_t Table_ID, uint8_t H_codes[16], uint8_t *Symbol_array):
+    length{Length}, type{Type}, table_ID{Table_ID}, symbol_array{Symbol_array} {
+        std::memcpy(h_codes, H_codes, 16);
+    }
+
+    uint16_t length;
+    uint8_t type;
+    uint8_t table_ID;
+    uint8_t h_codes[16];
+    uint8_t *symbol_array;
+};
+
+class DCTable{
+
+};
+
+class HTable{
+
+};
+
+JFIF_header *header;
 
 std::ifstream open_image(std::filesystem::path p);
 uint8_t find_marker(std::ifstream *image);
 JFIF_header *read_header(std::ifstream *image);
-QTable *read_QTable(std::ifstream *image)
-DCTable *read_DCTable(std::ifstream *image, bool ProgressiveDCTable)
-HTable *read_HTable(std::ifstream *image)
-void *read_comment(std::ifstream *image)
+QTable *read_QTable(std::ifstream *image);
+DCTable *read_DCTable(std::ifstream *image, bool ProgressiveDCTable);
+HTable *read_HTable(std::ifstream *image);
+void *read_comment(std::ifstream *image);
 
 int main(){
     std::filesystem::path p = "..\\example\\cat.jpg";
-    std::ifstream image {p};
-
-    cur_byte = image.get();
-
+    std::ifstream image = open_image(p);
+    
+    image.read(reinterpret_cast<char*>(&cur_byte), 1);
     //Find start of image
     if((cur_marker = find_marker(&image)) != 0xD8){
         std::cout << "Jpeg not detected\n";
@@ -50,24 +74,31 @@ int main(){
     while((cur_marker = find_marker(&image)) != 0xD9){ //0xD9 indicating end of image
         switch(cur_marker){
             case 0xE0:
-                JFIF_header *header = read_header(&image);
+                std::cout << "Header found\n";
+                header = read_header(&image);
                 break;
             case 0xDB:
+                std::cout << "Quantized table found\n";
                 //Read and Store Quantization Table
                 break;
             case 0xC0:
+                std::cout << "BaseLine DCT found\n";
                 //Read and Store Baseline DCT
                 break;
             case 0xC2:
+                std::cout << "Progressive DCT found\n";
                 //Read and Store Progressive DCT
                 break;
             case 0xC4:
+                std::cout << "Huffman found\n";
                 //Read and Store one or more Huffman tables
                 break;
             case 0xDD:
+                std::cout << "Restart found\n";
                 //Defines Restart Interval
                 break;
             case 0xDA:
+                std::cout << "Scan of the image found\n";
                 //Top to Bottom Scan of the image
                 /* Begins a top-to-bottom scan of the image.
                  In baseline DCT JPEG images, there is generally
@@ -77,56 +108,63 @@ int main(){
                  and is immediately followed by entropy-coded data. 
                 */
                 break;
+            /*
             case 0xDn: //n from 0 to 7
                 //Restart
-                /*
+                
                 Inserted every r macroblocks, where r is the restart
                 interval set by a DRI marker. Not used if there was 
                 no DRI marker. The low three bits of the marker code
                 cycle in value from 0 to 7.
-                */
+                
                 break;
+            
             case 0xEn: //For Application specific header that are not 0xE0
 
                 break;
+            */
             case 0xFE:
+                std::cout << "Comment found\n";
                 //contains a comment
                 break;
         }
     }
+    if(cur_marker == 0xD9)
+        printf("Position = %llu\nCur_marker = %d\n", position, cur_marker);
 }
 
 std::ifstream open_image(std::filesystem::path p){
     assert(exists(p));
-    return std::ifstream {p};
+    return std::ifstream (p, std::ios::binary);
 }
 
 uint8_t find_marker(std::ifstream *image){
-    while(!image->eof()){
+    while(image->peek() != EOF){
         if(cur_byte != 0xff){
-            cur_byte = image->get();
+            image->read(reinterpret_cast<char*>(&cur_byte), 1);
+            position++;
         }
         else{
-            cur_byte = image->get();
+            image->read(reinterpret_cast<char*>(&cur_byte), 1);
+            position++;
             if(cur_byte != 0x00){
-                marker = cur_byte;
-                cur_byte = image->get();
-                break;
+                cur_marker = cur_byte;
+                image->read(reinterpret_cast<char*>(&cur_byte), 1);
+                return cur_marker;
             }
-            cur_byte = image->get();
+            image->read(reinterpret_cast<char*>(&cur_byte), 1);
         }
     }
-    return marker;
+    return cur_marker;
 }
 
 JFIF_header *read_header(std::ifstream *image){
     uint16_t length = cur_byte;
-    cur_byte = image->get();
+    image->read(reinterpret_cast<char*>(&cur_byte), 1);
     length = (length << 8) + cur_byte;
     
     char *header = new char[length - 1];
-    image->get(header, (std::streamsize) length);
-
+    image->read(header, (std::streamsize) length);
     uint64_t Identfier = 0;
     for(int i = 0; i < 5; i++)
         Identfier = (Identfier << 8) + *((uint8_t *) header + i);
