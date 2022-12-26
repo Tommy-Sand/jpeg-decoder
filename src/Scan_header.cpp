406bd8035f7768908e5ca45c267a7832077acc83
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-12-26 14:41:45 -0700

    Finished decode Scan_header.cpp

---
+++ src/Scan_header.cpp
@@ -0,0 +1,23 @@
+#include "JPEG_decoder.hh"
+
+Scan_header::Scan_header(uint8_t *data): data{data} {
+    uint16_t pos = 1;
+
+    this->length = (uint16_t) *(data + (++pos));
+    this->length += *(data + (++pos));
+
+    this->num_chans = *(data + (++pos));
+
+    this->chan_specifiers = new struct Chan_specifier[this->num_chans];
+    for(int i = 0; i < this->num_chans; i++){
+        this->chan_specifiers[i].componentID = *(data + (++pos));
+        this->chan_specifiers[i].Huffman_DC = *(data + (++pos)) >> 4;
+        this->chan_specifiers[i].Huffman_AC = *(data + pos) & 0xF;
+    }
+
+    this->spectral_start = *(data + (++pos));
+    this->spectral_end = *(data + (++pos));
+
+    this->prev_approx = *(data + (++pos)) >> 4;
+    this->succ_approx = *(data + pos) & 0xF;
+}
